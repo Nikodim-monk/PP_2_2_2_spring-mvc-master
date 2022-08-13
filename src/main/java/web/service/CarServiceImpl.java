d65523479f69f9e7572b1c072718573c2fcd1b24
Nikodim-monk <lr1975@yandex.ru>
2022-08-14 00:56:28 +0400

    переписал

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,14 +1,16 @@
 package web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import web.dao.CarDao;
-import web.dao.CarDaoImpl;
 import web.model.Car;
 
 import java.util.List;
 
+@Component
 public class CarServiceImpl implements CarService {
-
-   CarDao carDao=new CarDaoImpl();
+    @Autowired
+    CarDao carDao;
 
     @Override
     public List<Car> getCars(String count) {
