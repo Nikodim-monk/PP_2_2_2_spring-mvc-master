358c791f72e455ff84dca77fe56da9c81818d7e6
Nikodim-monk <lr1975@yandex.ru>
2022-08-13 21:11:57 +0400

    переписал

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,19 +3,17 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
 import web.service.CarServiceImpl;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class CarController {
 
     @GetMapping("/cars")
-    public String printCars(HttpServletRequest request, ModelMap model) {
+    public String printCars(@RequestParam(value = "count", required = false) String count, ModelMap model) {
         CarService carService = new CarServiceImpl();
-        model.addAttribute("cars",carService.getCars(request.getParameter("count")));
+        model.addAttribute("cars", carService.getCars(count));
         return "cars";
     }
-
 }
